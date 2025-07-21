23681107644e19e409c97918fc6e52700ededdf8
nice <yejunhao@gitlab.com>
2025-07-21 16:58:58 +0800

    测试下推

--- server/logic/ConfigUpdate.cpp
+++ server/logic/ConfigUpdate.cpp
@@ -3,6 +3,7 @@
 #include "ILogic.h"
 #include "config_update.pb.h"
 #include "msg_id.pb.h"
+#include "notify.pb.h"
 
 ConfigUpdate::ConfigUpdate(Busd* busd) : ILogic(busd) {}
 
@@ -17,6 +18,15 @@ void ConfigUpdate::onConfigRequest(const NetPack& pPack)
     PROCESS_NETPACK_BEGIN(cs::ConfigUpdate);
 
     ILOG << request->update_file(0);
+
+    for(int32_t i; i < 5; i++)
+    {
+        cs::Notify notify;
+        notify.mutable_config_update_output()->set_context("updateing...");
+        busd_->sendToClient(uid, MSGID::SC_NOTIFY, notify);
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
     response->set_err(ErrorCode::Error_success);
 
